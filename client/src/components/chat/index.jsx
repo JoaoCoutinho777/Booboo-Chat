95830217c00b2f623342e2991f44af743b2fd5c1
jac <joao.coutinho@sebasi.pt>
2023-07-04 16:26:34 +0100

    feat: StandardMessageForm component add

---
+++ client/src/components/chat/index.jsx
@@ -0,0 +1,23 @@
+import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from 'react-chat-engine-advanced';
+import Header from '../customHeader';
+import StandardMessageForm from "../customMessageForms/StandardMessageForm";
+
+const Chat = () => {
+    const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID,"testuser","123")
+  return (
+    <div style={{flexBasis: "100%"}}>
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow 
+            {...chatProps} 
+            style={{height: "100vh"}}
+            renderChatHeader={(chat) => <Header chat={chat} />}
+            renderMessageForm={(props) => {
+              return (
+                <StandardMessageForm props={props} activeChat={chatProps.chat} />
+              )
+            }}/>
+    </div>
+  );
+};
+
+export default Chat;
